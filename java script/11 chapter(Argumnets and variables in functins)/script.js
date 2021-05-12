35f91beba85e9a95b91c98fe49dff1ec913c64f9
Hiral patel <hiralm227@gmail.com>
2021-05-11 23:36:16 -0400

    getting  multiple values in functions

--- java script/11 chapter(Argumnets and variables in functins)/script.js
+++ java script/11 chapter(Argumnets and variables in functins)/script.js
@@ -6,3 +6,31 @@
  
 
 
+function getArea(width , hight ){
+
+return width * hight 
+
+
+}
+
+var a = getArea(3,5)
+document.getElementById("cool").innerHTML = a ;
+
+
+//------------------------------------------------------
+
+
+wallWidth = 3;
+wallHight = 5;
+
+function getArea2(){
+
+    return  wallWidth *  wallHight
+    
+    
+    }
+
+ var b = getArea2(wallWidth , wallHight);
+
+
+ document.getElementById("cool2").innerHTML = b ;
