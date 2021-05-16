bd64a06f5e73412e96c7ced8a53045c755a6a460
Hiral patel <hiralm227@gmail.com>
2021-05-15 23:21:19 -0400

    constuctor notation

--- java script/20 chapter (Object Method DOT notation  )/script.js
+++ java script/20 chapter (Object Method DOT notation  )/script.js
@@ -1,8 +1,20 @@
 chosenPet = "Rocky";
 
+// this is an object method 
+// creating litral notation 
+// the  info inside curly braces are called object properties 
+
+
+
+// this pet name is called object 
 pet ={
 
+  // object [properties are called with : 
+  
   dogs: ["Mackie", "Bernice", "Cookie Monster", "Spot"],
+
+  // this dogs and cats are keys 
+
   cats: ["Astrid", "Rocky","Lulu", "Fluffy", "Mouser"],
 
     apptMessage: function () {
@@ -14,7 +26,11 @@ pet ={
 function dog(){
 
   console.log("we have this pet"  + "   "+ chosenPet  + " in dog list " )
+  
+  // we can use dot (.) method to use object keys ... or values ... or properties 
   pet.apptMessage()
+
+
 }
 
 function cat(){
@@ -22,6 +38,9 @@ function cat(){
   console.log(" we have this cat pet " + "   " + pet.cats[1]  + "in cat list " )
   pet.apptMessage()
 }
+
+
+// this    * include *    method is used to find same vlaues ot of long array ..
 
 
 if (pet.dogs.includes(chosenPet)) {
@@ -94,3 +113,42 @@ cars()
 
 
 
+var cool = "shivansh";
+
+stuff = {
+
+         girls:["shital","hiral","priya","pinky"],
+
+         boys:["rocky","shiva","shivansh","denish","vishu"]
+
+}
+
+function girls(){
+  console.log(cool + "" + "this name is in girls function ")
+
+    var  girlsEl = document.getElementById("demo3")
+     girlsEl.innerHTML = cool + "" + "this name is in girls function "
+
+
+}
+function boys(){
+
+  console.log(cool + " " + " this name is in boys function ")
+
+  var  boysEl = document.getElementById("demo3")
+     boysEl.innerHTML = cool + "     " + " this name is in boys  function !!!!!"
+
+
+
+}
+
+if(stuff.girls.includes(cool)){
+
+  girls()
+
+
+}else if(stuff.boys.includes(cool)){
+
+  boys()
+
+} else("error ")
