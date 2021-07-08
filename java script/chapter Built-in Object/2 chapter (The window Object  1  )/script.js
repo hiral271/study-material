c9d5373eabc9db9ac16e44afffb4f6a11aa5fe20
Hiral patel <hiralm227@gmail.com>
2021-07-07 21:32:35 -0400

    practise

---
+++ java script/chapter Built-in Object/2 chapter (The window Object  1  )/script.js
@@ -0,0 +1,36 @@
+
+// the window object represent the current browser page or tab . its contains other object that tell you about browser
+
+
+
+//---------------------(windows object properties)--------------------------//
+
+
+console.log(window.innerHeight)
+console.log(window.innerWidth)
+console.log(window.pageXOffset)                                                         
+console.log(window.pageYOffset)
+console.log(window.screenX)
+console.log(window.screenY)
+console.log(window.location)
+console.log(window.document)
+console.log(window.history)
+console.log(window.history.length)
+console.log(window.screen.width)
+console.log(window.screen.height)
+
+
+
+///____________________(Window object Method  )_____________________________________
+
+// this method are built to use  with windows  ==
+
+//  window.alert("helllo !!!! hiral    !!!!!   ")
+
+//  window.open(` https://www.google.com/`)
+
+// window.print()
+
+// we use window. before window method 
+ 
+
