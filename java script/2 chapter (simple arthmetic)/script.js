75671cee4fc2ff6bb2e620a564d4e3b44a1d5fdc
Hiral m patel <hiralm227@gmail.com>
2021-07-15 19:49:31 -0400

    delete update

--- java script/2 chapter (simple arthmetic)/script.js
+++
@@ -1,6 +0,0 @@
-var price =5;
-var quantity=15;
-var total= price * quantity;
-
-var el = document.getElementById('cost')
-el.textContent=  '$' + total;
--- java script/chapter Built-in Object/10 chapter ( Global object (Math Object))/script.js
+++
@@ -1,9 +0,0 @@
-var a = Math.PI
-piEl = document.getElementById("pi").innerHTML = a
-
-
-var c = 12.50;
-var b = Math.round(c)
-
-roundEl = document.getElementById("round").innerHTML = b
-
